0e2f5afab0fe469a26acb4a2a215cffdb831ee3a
Vishal Naik <vishal@vishal-laptop.(none)>
2011-10-27 13:33:44 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Askjeeves::Application.config.secret_token = 'c102f84fa1450511eda84e1daf616fcfed72657aed4ff4f6e1a4189c2146cca06e565c9d6c8290f22e15d18221faec3f17ec6a589b16e02c0e73114f82f12f4a'
